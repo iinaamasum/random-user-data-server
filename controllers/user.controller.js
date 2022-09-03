94c8e8fa3cad8263bda4cdddf57fc07ec9017fa4
iinaamasum <iinaamasum@gmail.com>
2022-09-03 08:36:52 +0600

    user controller testing with dummmy data

---
+++ controllers/user.controller.js
@@ -0,0 +1,4 @@
+const userData = require('../userData/userData.json');
+console.log(JSON.stringify(userData));
+
+module.exports.getRandomUser = (req, res, next) => {};
--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('first');
+const express = require('express');
+const app = express();
+const port = 4000;
+
+const userData = require('./userData/userData.json');
+console.log(JSON.stringify(userData));
+
+app.all('/', (req, res) => {
+  res.status(200).send('Server is running');
+});
+
+app.listen(port, () => {
+  console.log(`random user app listening on port ${port}`);
+});
