fc0f1f7a26546da7d4e494f49405be02e7cc75fa
iinaamasum <iinaamasum@gmail.com>
2022-09-03 22:55:26 +0600

    vecel config done

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const data = require('./userData/userDataParse');
 const userRouter = require('./routes/user.route.js');
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(express.json());
